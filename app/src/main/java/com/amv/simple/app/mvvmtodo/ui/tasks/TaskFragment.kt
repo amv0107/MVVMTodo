8e57edcc3a57983b8f5b4e2ce508f90cd20bd51f
Максим Андрейченко <94169040+amv0107@users.noreply.github.com>
2024-02-22 21:58:12 +0200

    end of part 6

--- app/src/main/java/com/amv/simple/app/mvvmtodo/ui/tasks/TaskFragment.kt
+++ app/src/main/java/com/amv/simple/app/mvvmtodo/ui/tasks/TaskFragment.kt
@@ -1,16 +1,21 @@
 package com.amv.simple.app.mvvmtodo.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.amv.simple.app.mvvmtodo.R
 import com.amv.simple.app.mvvmtodo.databinding.FragmentTasksBinding
+import com.amv.simple.app.mvvmtodo.util.onQueryTextChange
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class TaskFragment:Fragment(R.layout.fragment_tasks) {
+class TaskFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TaskViewModel by viewModels()
 
@@ -32,5 +37,47 @@ class TaskFragment:Fragment(R.layout.fragment_tasks) {
         viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
         }
+
+        setHasOptionsMenu(true)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChange {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sort_by_name -> {
+                viewModel.sortOrder.value = SortOrder.BY_NAME
+                true
+            }
+
+            R.id.action_sort_by_date_created -> {
+                viewModel.sortOrder.value = SortOrder.BY_DATE
+                true
+            }
+
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                viewModel.hideCompleted.value = item.isChecked
+                true
+            }
+
+            R.id.action_delete_all_completed_tasks -> {
+
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
--- app/src/main/java/com/amv/simple/app/mvvmtodo/ui/tasks/TaskViewModel.kt
+++ app/src/main/java/com/amv/simple/app/mvvmtodo/ui/tasks/TaskViewModel.kt
@@ -4,12 +4,31 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.amv.simple.app.mvvmtodo.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
 class TaskViewModel @Inject constructor(
     private val taskDao: TaskDao
-): ViewModel() {
+) : ViewModel() {
+    val searchQuery = MutableStateFlow("")
 
-    val tasks = taskDao.getTasks().asLiveData()
+    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
+    val hideCompleted = MutableStateFlow(false)
+
+    private val taskFlow = combine(
+        searchQuery,
+        sortOrder,
+        hideCompleted
+    ) { query, sortOrder, hideCompleted ->
+        Triple(query, sortOrder, hideCompleted)
+    }.flatMapLatest { (query, sortOrder, hideCompleted) ->
+        taskDao.getTasks(query, sortOrder, hideCompleted)
+    }
+
+    val tasks = taskFlow.asLiveData()
 }
+
+enum class SortOrder { BY_NAME, BY_DATE }
